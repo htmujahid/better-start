de714860ad6ea74d6c9c2a46f8cb0339479688dd
Talha Mujahid <htmujahid@gmail.com>
2025-05-02 18:17:53 +0500

    fix: vercel cors issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -62,6 +62,7 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg', // or "mysql", "sqlite"
   }),
+  trustedOrigins: [process.env.VERCEL_URL!],
   plugins: [
     admin({
       ac,
