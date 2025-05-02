bcf17ad40a92f99c14caee7b2cf66bc13dd9fa75
Talha Mujahid <htmujahid@gmail.com>
2025-05-02 17:55:51 +0500

    fix: pnpm lock

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,10 @@ import postgres from 'postgres'
 
 import * as schema from './schema'
 
-const queryClient = postgres(process.env.DATABASE_URL!)
+const queryClient = postgres(process.env.DATABASE_URL!, {
+  ssl: {
+    rejectUnauthorized: false,
+  }
+})
 
 export const db = drizzle({ client: queryClient, schema })
